bb5368615a43459dcfd936520e3cfc67e58cfa2f
Dawrld <52791111+Dawrld@users.noreply.github.com>
2020-11-13 01:24:08 +0600

    Update linear_search.py

--- linear-search/linear_search.py
+++ linear-search/linear_search.py
@@ -1,4 +1,8 @@
 def linear_search(lst, to_find):
+  for i in lst:
+    if i==to_find: 
+      return lst.index(i)
+    return -1
   # search for the element to_find inside lst
   # if found, return index of element
   # else return -1
